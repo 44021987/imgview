4e049c512bc4c23e1da7542de881140479673d6c
44021987 <44021987@qq.com>
2020-10-15 15:55:31 +0800

    [perf]优化多图预览模式

--- src/imgview.js
+++ src/imgview.js
@@ -1,8 +1,10 @@
 /**
+ * @description 简单的图片预览
  * @param {Object} opts
  * @param {Boolean} opts.maskClose 点击遮罩层是否关闭预览
  * @param {Number} opts.maxScale 最大缩放值倍数
  * @param {Number} opts.minScale 最小缩放值倍数
+ * @param {Function} opts.downloadFormat 下载时用
  * 多图预览 {data: Array, index: Number}
  * 单个预览 {src: 'String src'}
  * @Author: xd
@@ -12,25 +14,34 @@
 import './css/imgview.css'
 import loading from './img/loading.gif'
 
-function $$(v, node) {
+function $$ (v, node) {
   node = node || document
   return node.querySelector(v)
 }
+// 默认配置
 const defaultOpts = {
   index: 0,
   data: [],
   maxScale: 3,
   minScale: 0.5,
   maskClose: false,
-  showDowmload: false
+  showDowmload: false,
+  downloadFormat: (src) => src
 }
-const isObject = opts => Object.prototype.toString.call(opts) === '[object Object]' 
+// 检测是否为对象类型
+const isObject = (opts) => Object.prototype.toString.call(opts) === '[object Object]'
 
-export const Imgview = function(opts) {
+/**
+ * @constructor
+ * @param {*} opts 配置
+ */
+export const Imgview = function (opts) {
   if (!isObject(opts)) throw new Error('arguments type error')
   for (const key in defaultOpts) {
-    if (opts[key] === undefined) opts[key] = defaultOpts[key]
+    if (opts[key] === undefined) { opts[key] = defaultOpts[key] }
   }
+  const { src, index, data, maskClose, maxScale, minScale } = opts
+  this.config = opts
   this.x = 0
   this.y = 0
   this.left = 0
@@ -40,14 +51,13 @@ export const Imgview = function(opts) {
   this.clientL = 0
   this.clientT = 0
   this.isMove = false
-  this.showDowmload = opts.showDowmload
   this.box = null
-  this.index = opts.index
-  this.data = opts.data
-  this.src = opts.src || this.data[this.index]
-  this.maskClose = opts.maskClose
-  this.maxScale = opts.maxScale
-  this.minScale = opts.minScale
+  this.data = data
+  this.index = this.currentIndex(index)
+  this.src = src || data[this.index]
+  this.maskClose = maskClose
+  this.maxScale = maxScale
+  this.minScale = minScale
   this.imgWrapWidth = 0
   this.imgWrapHeight = 0
   this.minScreen = 1200
@@ -56,42 +66,46 @@ export const Imgview = function(opts) {
   this.isIframe = false
   this.init()
 }
-Imgview.prototype.init = function() {
-  var that = this
+Imgview.prototype.init = function () {
   this.setIsIframe()
-  this.createImgview(function() {
-    that.bindClickEvent()
-    that.bindMove()
-    that.bindSelect()
+  this.createImgview(() => {
+    this.bindClickEvent()
+    this.bindMove()
+    // 绑定图片切换事件
+    this.data && this.data.length && this.bindSelect()
   })
-  this.remove()
+  this.bindRemove()
 }
-Imgview.prototype.setIsIframe = function() {
-  try{
+
+// 判断当前是否在iframe中打开
+Imgview.prototype.setIsIframe = function () {
+  try {
     this.isIframe = (self != top && !!top.Imgview)
-  } catch(e) {
+  } catch (e) {
     this.isIframe = false;
   }
 }
-Imgview.prototype.createImgview = function(fn) {
-  var clientW = document.documentElement.clientWidth || document.body.clientWidth
-  var clientH = document.documentElement.clientHeight || document.body.clientHeight
-  var oWrap = clientW > this.minScreen ? parseInt(clientW * 0.6 - 100) : parseInt(clientW * 0.8 - 100)
-  var oWrapHeight = clientH - 100
-  var oStyle = 'widht: 100%'
-  var img = new Image()
-  var imgbox = this.createdDom()
-  var that = this
+
+// 创建dom
+Imgview.prototype.createImgview = function (fn) {
+  const clientW = document.documentElement.clientWidth || document.body.clientWidth
+  const clientH = document.documentElement.clientHeight || document.body.clientHeight
+  const oWrap = clientW > this.minScreen ? parseInt(clientW * 0.6 - 100) : parseInt(clientW * 0.8 - 100)
+  const oWrapHeight = clientH - 100
+  const oStyle = 'widht: 100%'
+  const img = new Image()
+  const imgbox = this.createdDom()
+  const that = this
   this.imgWrapWidth = oWrap
   this.imgWrapHeight = oWrapHeight
   img.src = this.src
-  img.onload = function() {
-    var x = this.naturalWidth || this.width
-    var y = this.naturalHeight || this.height
-    var imgWidth = 0
-    var imgHeight = 0
-    var ratio = 1
-    var tagImag = imgbox.querySelector('img')
+  img.onload = function () {
+    const x = this.naturalWidth || this.width
+    const y = this.naturalHeight || this.height
+    const tagImag = imgbox.querySelector('img')
+    let imgWidth = 0
+    let imgHeight = 0
+    let ratio = 1
     if (!x || !y) return
     ratio = x / y
     if (ratio < 1) {
@@ -113,9 +127,11 @@ Imgview.prototype.createImgview = function(fn) {
     fn && fn.call(that, oStyle)
   }
 }
-Imgview.prototype.createdDom = function() {
-  var clientW = document.documentElement.clientWidth || document.body.clientWidth
-  var style = clientW > this.minScreen ? '' : 'width:' + this.imgWidthPersent
+
+// 创建预览层dom
+Imgview.prototype.createdDom = function () {
+  const clientW = document.documentElement.clientWidth || document.body.clientWidth
+  const style = clientW > this.minScreen ? '' : 'width:' + this.imgWidthPersent
   if (this.box) {
     if (this.isIframe) {
       top.document.body.removeChild(this.box)
@@ -123,25 +139,27 @@ Imgview.prototype.createdDom = function() {
       document.body.removeChild(this.box)
     }
   }
-  var oBox = document.createElement('div')
-  var str =
-    '<div class="imgview-view" style="'+style+'"><div class="btn-wrap">' +
-    '<button id="btn-add" class="imgview-btn scale-btn" title="放大">+</button>' +
-    '<button id="btn-restore" class="imgview-btn scale-btn" title="还原">1</button>' +
-    '<button id="btn-reduce" class="imgview-btn scale-btn" title="缩小">-</button>'
-    if (this.showDowmload === true) {
-    str += '<a class="imgview-btn dowmload-btn" title="下载" href="'+this.src+'" download="'+this.src.split('/').pop()+'"></a>'
+  const oBox = document.createElement('div')
+  const { showDowmload, downloadFormat } = this.config
+  let str =
+    '<div class="imgview-view" style="' + style + '"><div class="btn-wrap">' +
+    '<button id="imgview-btn-big" class="imgview-btn scale-btn" title="放大">+</button>' +
+    '<button id="imgview-btn-reset" class="imgview-btn scale-btn" title="还原">1</button>' +
+    '<button id="imgview-btn-small" class="imgview-btn scale-btn" title="缩小">-</button>'
+  if (showDowmload === true) {
+    str += '<a class="imgview-btn dowmload-btn" title="下载" href="' + downloadFormat(this.src, this.index) + '" download="' + this.src.split('/').pop() + '"></a>'
   }
   if (this.data.length) {
-    str +=
-      '<button id="btn-prev" class="imgview-btn prev"><</button>' +
-      '<button id="btn-next" class="imgview-btn next">></button>'
+    str += `
+    <button id="imgview-btn-prev" class="imgview-btn prev ${this.index === 0 ? 'imgview-not-allowed' : ''}"><</button>
+    <button id="imgview-btn-next" class="imgview-btn next  ${this.index === this.data.length - 1 ? 'imgview-not-allowed' : ''}">></button>
+    `
   }
   str +=
     '</div>' +
     '<div class="imgview-inner">' +
     '<div class="imgview-wrap">' +
-    '<img class="imgview-show" style="width: 100px" src=' +loading +' />' +
+    '<img class="imgview-show" style="width: 100px" src=' + loading + ' />' +
     '</div>' +
     '</div>' +
     '<div class="del-box">×</div></div>'
@@ -157,12 +175,12 @@ Imgview.prototype.createdDom = function() {
   this.box = oBox
   return oBox
 }
-Imgview.prototype.remove = function() {
-  var that = this;
-  this.box.onclick = function(e) {
-    var target = e.target
+Imgview.prototype.bindRemove = function () {
+  const that = this;
+  this.box.onclick = function (e) {
+    const target = e.target
     if (target === $$('.del-box', that.box) || (that.maskClose === true && target === this)) {
-      if (that.isIframe) { 
+      if (that.isIframe) {
         top.document.body.removeChild(this)
       } else {
         document.body.removeChild(this)
@@ -171,24 +189,39 @@ Imgview.prototype.remove = function() {
     }
   }
 }
-Imgview.prototype.bindSelect = function() {
-  var oBtns = $$('.imgview-box .btn-wrap', this.box)
-  var oImg = $$('.imgview-show', this.box)
-  var _this = this
-  var changeImg = function(target, el) {
-    var num = 1
-    var el = null
-    if (target.id !== 'btn-prev' && target.id !== 'btn-next') return
-    num = target.id === 'btn-prev' ? -1 : 1
-    el = target.id === 'btn-prev' ? $$('#btn-next', _this.box) : $$('#btn-prev', _this.box)
+
+// 当前显示的图片index
+Imgview.prototype.currentIndex = function (num) {
+  const maxLen = this.data.length - 1
+  return num < 0 ? 0 : (num > maxLen ? maxLen : num)
+}
+
+// 多图片时，绑定切换按钮
+Imgview.prototype.bindSelect = function () {
+  const BtnWrap = $$('.imgview-box .btn-wrap', this.box)
+  const oImg = $$('.imgview-show', this.box)
+  const _this = this
+  const btns = BtnWrap.querySelectorAll('.imgview-btn')
+  const changeImg = function (target) {
+    // 上一张按钮
+    const isPrev = target.id === 'imgview-btn-prev'
+    // 下一张按钮
+    const isNext = target.id === 'imgview-btn-next'
+    if (!isPrev && !isNext) return
+    const num = isPrev ? _this.index - 1 : _this.index + 1
+    // 获取按钮
+    // const el = $$(`#imgview-btn-${isPrev ? 'prev' : 'next'}`, _this.box)
+    // 第一张或者最后一张时隐藏按钮
+    const maxLen = _this.data.length - 1
+    _this.index = _this.currentIndex(num)
+    btns.forEach((v) => {
+      v.classList.remove('imgview-not-allowed')
+    })
     if (
-      (num === -1 && _this.index === 0) ||
-      (num === 1 && _this.index === _this.data.length - 1)
+      (isPrev && _this.index === 0) ||
+      (isNext && _this.index === maxLen)
     ) {
-      target.style.opacity = '0'
-    } else {
-      el.style.opacity = '1'
-      _this.index += num
+      target.classList.add('imgview-not-allowed')
     }
     _this.scale = 1
     oImg.style.left = 0
@@ -199,14 +232,16 @@ Imgview.prototype.bindSelect = function() {
     _this.init()
   }
 
-  oBtns.addEventListener('click', function(e) {
+  BtnWrap.addEventListener('click', function (e) {
     changeImg(e.target)
   })
 }
-Imgview.prototype.bindClickEvent = function() {
-  var _this = this
-  var oImg = $$('.imgview-show', this.box)
-  var changeTransform = function() {
+
+// 绑定事件
+Imgview.prototype.bindClickEvent = function () {
+  const _this = this
+  const oImg = $$('.imgview-show', this.box)
+  var changeTransform = function () {
     if (_this.scale <= 1) {
       oImg.style.cursor = ''
     } else {
@@ -214,18 +249,21 @@ Imgview.prototype.bindClickEvent = function() {
     }
     _this.setScale(oImg, _this.scale, _this.scale)
   }
-  $$('#btn-add', this.box).addEventListener('click', function() {
+  // 放大
+  $$('#imgview-btn-big', this.box).addEventListener('click', function () {
     _this.scale += 0.35
     if (_this.scale >= _this.maxScale) _this.scale = _this.maxScale
     changeTransform()
   })
-  $$('#btn-restore', this.box).addEventListener('click', function() {
+  // 还原
+  $$('#imgview-btn-reset', this.box).addEventListener('click', function () {
     _this.scale = 1
     oImg.style.left = 0
     oImg.style.top = 0
     changeTransform()
   })
-  $$('#btn-reduce', this.box).addEventListener('click', function() {
+  // 缩小
+  $$('#imgview-btn-small', this.box).addEventListener('click', function () {
     _this.scale -= 0.35
     if (_this.scale <= _this.minScale) _this.scale = _this.minScale
     oImg.style.left = 0
@@ -234,17 +272,19 @@ Imgview.prototype.bindClickEvent = function() {
   })
 }
 
-Imgview.prototype.setScale = function(el, x, y) {
+Imgview.prototype.setScale = function (el, x, y) {
   el.style.transform = 'scale3d(' + x + ', ' + y + ', 1)'
   el.style.WebkitTransform = 'scale3d(' + x + ', ' + y + ', 1)'
 }
-Imgview.prototype.bindMove = function() {
-  var _this = this
-  var doc = this.isIframe ? top.document : document
-  var oImg = $$('.imgview-show', this.box)
-  oImg.onmousedown = function(e) {
-    var imgWidth = this.offsetWidth * _this.scale
-    var imgHeight = this.offsetHeight * _this.scale
+
+// 拖拽
+Imgview.prototype.bindMove = function () {
+  const _this = this
+  const doc = this.isIframe ? top.document : document
+  const oImg = $$('.imgview-show', this.box)
+  oImg.onmousedown = function (e) {
+    const imgWidth = this.offsetWidth * _this.scale
+    const imgHeight = this.offsetHeight * _this.scale
     _this.x = e.clientX
     _this.y = e.clientY
     _this.isMove = true
@@ -255,14 +295,14 @@ Imgview.prototype.bindMove = function() {
     e.preventDefault()
   }
 
-  oImg.onmousemove = function(e) {
+  oImg.onmousemove = function (e) {
     // 正在移动或缩放不超过1倍的不改变位置
     if (!_this.isMove || _this.scale <= 1) return
-    var imgWidth = this.offsetWidth * _this.scale
-    var imgHeight = this.offsetHeight * _this.scale
-    var x1 = e.clientX - _this.x + _this.left
-    var y1 = e.clientY - _this.y + _this.top
-    var getMoveDis = function(num, clientDis) {
+    const imgWidth = this.offsetWidth * _this.scale
+    const imgHeight = this.offsetHeight * _this.scale
+    const x1 = e.clientX - _this.x + _this.left
+    const y1 = e.clientY - _this.y + _this.top
+    const getMoveDis = function (num, clientDis) {
       if (num > clientDis || num < -clientDis) {
         num > 0 ? (num = clientDis) : (num = -clientDis)
       }
@@ -274,13 +314,13 @@ Imgview.prototype.bindMove = function() {
     }
     if (imgWidth > _this.imgWrapWidth) {
       let dis = getMoveDis(x1, _this.clientL)
-      const maxLeft = (imgWidth - _this.imgWrapWidth)/2
+      const maxLeft = (imgWidth - _this.imgWrapWidth) / 2
       if (dis > maxLeft) dis = maxLeft
       if (dis < -maxLeft) dis = -maxLeft
       this.style.left = dis + 'px'
     }
   }
-  doc.onmouseup = function(e) {
+  doc.onmouseup = function (e) {
     _this.isMove = false
     _this.x = 0
     _this.y = 0
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const mode = process.env.NODE_ENV
-
+// 是否为开发环境
+// const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
   entry: {
     imgview: './src/imgview.js'
@@ -12,6 +14,26 @@ module.exports = {
     chunkFilename: '[name].min.js',
     path: path.resolve(__dirname, 'demo')
   },
+  // optimization: {
+  //   minimizer: [
+  //     new UglifyJsPlugin({
+  //       uglifyOptions: {
+  //         ecma: 5,
+  //         // 其它优化选项 https://segmentfault.com/a/1190000010874406
+  //         warnings: false,
+  //         compress: {
+  //           drop_console: true,
+  //           drop_debugger: true,
+  //           // 生产环境,如果是非debug包删除所有的console
+  //           pure_funcs: [!isDev ? 'console.log' : '']
+  //         }
+  //       },
+  //       cache: false, // 启动缓存
+  //       sourceMap: false,
+  //       parallel: 4 // 启用多进程并行运行
+  //     })
+  //   ]
+  // },
   module: {
     rules: [
       {
@@ -26,8 +48,8 @@ module.exports = {
         }
       },
       {
-        test:/\.css$/,   
-        use:["style-loader","css-loader"]
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png|jpg|gif)$/i,
